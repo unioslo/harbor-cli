e112e93d9d9782b3a63e442e7981312ab3faf87b
pederhan <pederhan@uio.no>
2022-12-20 09:57:28 +0100

    Add harborapi.ext as possible schema source model

--- harbor_cli/output/schema.py
+++ harbor_cli/output/schema.py
@@ -29,14 +29,17 @@ class Schema(BaseModel, Generic[T]):
     """A schema for (de)serializing data (JSON, YAML, etc.)"""
 
     version: str = "1.0.0"  # TODO: use harborapi.models.SemVer?
-    data: T | list[T]
     type_: str | None = None  # should only be None if empty list
+    data: T | list[T]
 
     class Config:
         extra = "allow"
 
     @root_validator
     def set_type(cls, values: dict[str, Any]) -> dict[str, Any]:
+        if values["type_"] is not None:
+            return values
+
         data = values.get("data")
         if isinstance(data, BaseModel):
             typ = data.__class__.__name__
@@ -62,22 +65,29 @@ class Schema(BaseModel, Generic[T]):
         and properties tied to the data type.
         """
         # TODO: add support for harborapi.ext
-        module = importlib.import_module("harborapi.models")
         if self.type_ is None:
             return self
-        try:
-            data_type = getattr(module, self.type_)
-            assert issubclass(data_type, HarborBaseModel)
-            if isinstance(self.data, list):
-                for i, item in enumerate(self.data):
-                    self.data[i] = data_type.parse_obj(item)
-            else:
-                self.data = data_type.parse_obj(self.data)
-        except (ImportError, TypeError, AttributeError):
-            logger.warning(
-                f"Unable to import data type {self.type_} from harborapi.models"
-            )
-        # TODO: catch AssertionError?
+
+        modules = ["harborapi.models", "harborapi.models.models", "harborapi.ext"]
+        for module_name in modules:
+            try:
+                module = importlib.import_module(module_name)
+                data_type = getattr(module, self.type_)
+                assert issubclass(data_type, HarborBaseModel)
+                if isinstance(self.data, list):
+                    for i, item in enumerate(self.data):
+                        self.data[i] = data_type.parse_obj(item)
+                else:
+                    self.data = data_type.parse_obj(self.data)
+                break  # signal success # TODO: just return self here?
+            except (ImportError, TypeError, AttributeError):
+                continue  # try next module
+            except AssertionError:
+                logger.warning(
+                    f"Data type {self.type_} from {module_name} is not a subclass of harborapi.models.BaseModel"
+                )
+        else:
+            logger.warning(f"Unable to import data type {self.type_} from {modules}")
         return self
 
     @classmethod
