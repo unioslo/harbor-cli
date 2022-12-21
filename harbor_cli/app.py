5b85c6b4a68322f85fbd33e24d1f4491b9a832bb
pederhan <pederhan@usit.uio.no>
2022-12-21 10:50:47 +0100

    Enable Rich markup mode

--- harbor_cli/app.py
+++ harbor_cli/app.py
@@ -6,4 +6,8 @@ app = typer.Typer(
     help="Harbor CLI",
     pretty_exceptions_show_locals=False,
     no_args_is_help=True,
+    # rich_markup_mode needs to be set here!
+    # Sub-commands will inherit this setting,
+    # but it has no effect if only set in sub-commands
+    rich_markup_mode="rich",
 )
