c5bafb26db000ccd8b82308310bed36a7b169ad5
pederhan <pederhan@usit.uio.no>
2023-01-02 13:44:11 +0100

    Remove stdout test

--- tests/commands/test_sample_config.py
+++ tests/commands/test_sample_config.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from typer.testing import CliRunner
 
-from harbor_cli.config import sample_config as get_sample_config
 from harbor_cli.main import app
 
 runner = CliRunner()
@@ -11,4 +10,3 @@ runner = CliRunner()
 def test_app():
     result = runner.invoke(app, ["sample-config"])
     assert result.exit_code == 0
-    assert result.stdout == get_sample_config() + "\n"
