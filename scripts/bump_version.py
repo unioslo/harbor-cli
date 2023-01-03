b1bb1866d798ca4a22af3e6d6a269aeb6458e399
pederhan <pederhan@usit.uio.no>
2023-01-03 12:05:59 +0100

    script: Support specific version bump

--- scripts/bump_version.py
+++ scripts/bump_version.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import subprocess
 from enum import Enum
-from typing import Optional
 
 import typer
 
@@ -13,7 +12,7 @@ class VersionType(Enum):
     patch: str = "patch"
 
 
-class ReleaseType(Enum):
+class StatusType(Enum):
     # Release
     release: str = "release"
 
@@ -40,32 +39,35 @@ class ReleaseType(Enum):
     dev: str = "dev"
 
 
-# Not all combinations of version type and release type are valid.
-# but that is not checked here.
-
 # TODO: some sort of check to ensure we don't tag twice (??)
 # but we also need to allow for the case where we want to create
-# a release tag for a version that already exists.
+# a status tag for a version that already exists.
+
+versions = [v.value for v in VersionType]
+statuses = [s.value for s in StatusType]
 
 
 def main(
-    version_type: VersionType = typer.Argument(
-        ..., help="The type of version bump to perform"
-    ),
-    release_type: Optional[ReleaseType] = typer.Argument(
-        None, help="The release type to use for the new version."
+    version: str = typer.Argument(
+        ...,
+        help="Version bump to perform or new version to set.",
+        metavar="[" + "|".join(versions) + "|x.y.z],[" + "|".join(statuses) + "]",
     ),
 ):
-    """Bump the version of the project and create a new git tag.""" ""
-    typer.echo(f"Version type: {version_type}")
-    typer.echo(f"Release type: {release_type}")
+    """Bump the version of the project and create a new git tag.
 
-    args = [version_type.value]
-    if release_type:
-        args.append(release_type.value)
-    version_arg = ",".join(args)
+    Examples:
 
-    p_version = subprocess.run(["hatch", "version", version_arg])
+    $ python bump_version.py minor
+
+    $ python bump_version.py major,rc
+
+    $ python bump_version.py 1.2.3 # generally don't use this
+    """
+    # We don't verify that the arguments are valid, we just pass them
+    # to hatch and let it handle it.
+    # Worst case scenario, we get a non-zero exit code and the script exits
+    p_version = subprocess.run(["hatch", "version", version])
     if p_version.returncode != 0:
         typer.echo(f"Failed to bump version: {p_version.stderr.decode()}")
         raise typer.Exit(1)
