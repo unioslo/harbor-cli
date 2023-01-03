f0fdf6c4c094b52f73795133b1d243f205f435e1
pederhan <pederhan@usit.uio.no>
2023-01-03 10:18:47 +0100

    Add LDAP commands

--- harbor_cli/commands/api/__init__.py
+++ harbor_cli/commands/api/__init__.py
@@ -8,6 +8,7 @@ from . import auditlog as auditlog
 from . import config as config
 from . import cve_allowlist as cve_allowlist
 from . import gc as gc
+from . import ldap as ldap
 from . import project as project
 from . import registry as registry
 from . import replication as replication
@@ -24,6 +25,7 @@ api_commands: list[typer.Typer] = [
     config.app,
     cve_allowlist.app,
     gc.app,
+    ldap.app,
     project.app,
     registry.app,
     replication.app,
---
+++ harbor_cli/commands/api/ldap.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+from typing import Optional
+
+import typer
+from harborapi.models.models import LdapConf
+
+from ...output.render import render_result
+from ...state import state
+from ...utils import inject_help
+from ...utils.args import model_params_from_ctx
+
+# Create a command group
+app = typer.Typer(
+    name="ldap",
+    help="LDAP configuration",
+    no_args_is_help=True,
+)
+search_cmd = typer.Typer(
+    name="search",
+    help="Search for users and groups in LDAP",
+    no_args_is_help=True,
+)
+app.add_typer(search_cmd)
+
+
+@app.command("ping")
+@inject_help(LdapConf)
+def ping(
+    ctx: typer.Context,
+    ldap_url: str = typer.Option(None, "--url"),
+    ldap_search_dn: str = typer.Option(None, "--search-dn"),
+    ldap_search_password: str = typer.Option(None, "--search-password"),
+    ldap_base_dn: str = typer.Option(None, "--base-dn"),
+    ldap_filter: str = typer.Option(None, "--filter"),
+    ldap_uid: str = typer.Option(None, "--uid"),
+    ldap_scope: int = typer.Option(None, "--scope"),
+    ldap_connection_timeout: int = typer.Option(None, "--timeout"),
+    ldap_verify_cert: bool = typer.Option(None, "--verify-cert"),
+) -> None:
+    """Ping LDAP service. Uses default configuration if none is provided."""
+    params = model_params_from_ctx(ctx, LdapConf)
+    if params:
+        conf = LdapConf(**params)
+    else:
+        conf = None
+    ldap_info = state.run(state.client.ping_ldap(conf))
+    render_result(ldap_info, ctx)
+
+
+# FIXME: this will never return more than one user...
+#        Should we rename to `user` and return an LdapUser instead
+#        of a list (len=1) of LdapUser?
+@search_cmd.command("users")
+def search_users(
+    ctx: typer.Context,
+    username: str = typer.Argument(..., help="Username to search for"),
+) -> None:
+    """Search for users in LDAP."""
+    result = state.run(state.client.search_ldap_users(username))
+    render_result(result, ctx)
+
+
+@search_cmd.command("groups")
+def search_groups(
+    ctx: typer.Context,
+    group_name: Optional[str] = typer.Option(None, help="Group name to search for"),
+    group_dn: Optional[str] = typer.Option(None, help="Group DN to search for"),
+) -> None:
+    """Search for groups in LDAP."""
+    if not group_name and not group_dn:
+        raise typer.BadParameter("One of --group-name or --group-dn is required")
+    result = state.run(state.client.search_ldap_groups(group_name, group_dn))
+    render_result(result, ctx)
