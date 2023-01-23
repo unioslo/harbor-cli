d11dfdee37b8c9f3092a461424ce321e968f7803
pederhan <pederhan@usit.uio.no>
2023-01-23 16:31:35 +0100

    Remove unused scanner create/update abstraction

--- harbor_cli/commands/api/scanner.py
+++ harbor_cli/commands/api/scanner.py
@@ -6,7 +6,6 @@ import typer
 from harborapi.models.models import ScannerRegistration
 from harborapi.models.models import ScannerRegistrationReq
 
-from ...exceptions import HarborCLIError
 from ...logs import logger
 from ...output.render import render_result
 from ...state import state
@@ -39,95 +38,6 @@ def get_csanner(
     """Get a specific scanner."""
     scanner = get_scanner(scanner_id)
     render_result(scanner, ctx)
-
-
-@inject_help(ScannerRegistrationReq)
-def _do_handle_scanner_modification(
-    ctx: typer.Context,
-    scanner_id: Optional[str] = typer.Argument(
-        None,
-        help="ID of the scanner to modify. Ignored for create.",
-    ),
-    name: str = typer.Option(
-        "",
-        "--name",
-    ),
-    url: str = typer.Option(
-        "",
-        "--url",
-    ),
-    description: Optional[str] = typer.Option(
-        None,
-        "--description",
-    ),
-    auth: Optional[str] = typer.Option(
-        None,
-        "--auth",
-    ),
-    access_credential: Optional[str] = typer.Option(
-        None,
-        "--access-credential",
-    ),
-    skip_cert_verify: Optional[bool] = typer.Option(
-        None,
-    ),
-    use_internal_addr: Optional[bool] = typer.Option(
-        None,
-    ),
-    disabled: bool = typer.Option(
-        False,
-        "--disabled",
-    ),
-) -> None:
-    # When creating, we require
-    if ctx.command.name == "create":
-        if scanner_id is not None:
-            raise typer.BadParameter("Unexpected argument SCANNER_ID")
-        if name == "":
-            raise typer.BadParameter("Missing required argument --name")
-        if url == "":
-            raise typer.BadParameter("Missing required argument --url")
-    elif ctx.command.name == "update":
-        if scanner_id is None:
-            raise typer.BadParameter("Missing required argument SCANNER_ID")
-
-    req = ScannerRegistrationReq(
-        name=name,
-        description=description,
-        url=url,
-        auth=auth,
-        access_credential=access_credential,
-        skip_cert_verify=skip_cert_verify,
-        use_internal_addr=use_internal_addr,
-        disabled=disabled,
-    )
-    # TODO: investigate which parameters the `parameters` field takes
-
-    # TODO: fix this shitshow
-    if ctx.command.name == "create":
-        location = state.run(state.client.create_scanner(req), "Creating scanner...")
-        render_result(location, ctx)
-        logger.info(f"Scanner created: {location}.")
-    elif ctx.command.name == "update":
-        assert scanner_id is not None  # type: ignore # mypy doesn't understand that we have checked this already
-        existing_scanner = state.run(
-            state.client.get_scanner(scanner_id), "Fetching current scanner..."
-        )
-        if existing_scanner is None:
-            raise HarborCLIError(f"Scanner with ID {scanner_id!r} does not exist.")
-
-        # Cast existing scanner to dict, update it with the new values and parse it back to a ScannerRegistrationReq
-        d = existing_scanner.dict()
-        d.update(req.dict(exclude_none=True, exclude_unset=True))
-        req = ScannerRegistrationReq.parse_obj(d)
-
-        state.run(
-            state.client.update_scanner(scanner_id, req),
-            "Updating scanner...",
-        )
-        logger.info(f"Scanner with ID {scanner_id!r} updated.")
-    else:
-        raise HarborCLIError(f"Unknown command {ctx.command.name}")
 
 
 # HarborAsyncClient.create_scanner()
