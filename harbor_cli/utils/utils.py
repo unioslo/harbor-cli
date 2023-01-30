c92d40841d0433f9e0db50f2f9682e28fb07fab8
pederhan <pederhan@usit.uio.no>
2023-01-30 12:51:10 +0100

    Add replace_none() iterable recursion

--- harbor_cli/utils/utils.py
+++ harbor_cli/utils/utils.py
@@ -3,23 +3,64 @@ that they don't need their own module."""
 from __future__ import annotations
 
 from typing import Any
+from typing import Iterable
+from typing import MutableMapping
+from typing import TypeVar
+
+MappingType = TypeVar("MappingType", bound=MutableMapping[str, Any])
 
 
-def replace_none(d: dict[str, Any], replacement: Any = "") -> dict[str, Any]:
+def replace_none(d: MappingType, replacement: Any = "") -> MappingType:
     """Replaces None values in a dict with a given replacement value.
-    Iterates recursively through nested dicts.
-    Lists of depth 1 are also iterated through.
+    Iterates recursively through nested dicts and iterables.
 
-    Does not support list of dicts yet.
-    Does not support containers other than dict and list.
+    Untested with iterables other than list, tuple, and set.
     """
+
     if d is None:
         return replacement
+
+    def _try_convert_to_original_type(
+        value: Iterable[Any], original_type: type
+    ) -> Iterable[Any]:
+        """Try to convert an iterable to the original type.
+        If the original type cannot be constructed with an iterable as
+        the only argument, return a list instead.
+        """
+        try:
+            return original_type(value)
+        except TypeError:
+            return list(value)
+
+    def _iter_iterable(value: Iterable[Any]) -> Iterable[Any]:
+        """Iterates through an iterable recursively, replacing None values."""
+        t = type(value)
+        v_generator = (item if item is not None else replacement for item in value)
+        values = []
+
+        for item in v_generator:
+            v = None
+            if isinstance(item, MutableMapping):
+                v = replace_none(item)
+            elif isinstance(item, str):  # don't need to recurse into each char
+                v = item  # type: ignore
+            elif isinstance(item, Iterable):
+                v = _iter_iterable(item)  # type: ignore
+            else:
+                v = item
+            values.append(v)
+        if values:
+            return _try_convert_to_original_type(values, t)
+        else:
+            return value
+
     for key, value in d.items():
-        if isinstance(value, dict):
+        if isinstance(value, MutableMapping):
             d[key] = replace_none(value)
-        elif isinstance(value, list):
-            d[key] = [item if item is not None else replacement for item in value]
+        elif isinstance(value, str):
+            d[key] = value
+        elif isinstance(value, Iterable):
+            d[key] = _iter_iterable(value)
         elif value is None:
             d[key] = replacement
     return d
