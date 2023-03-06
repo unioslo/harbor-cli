d2039ba6b74b9fcb61e1694535660eb890675c20
pederhan <pederhan@usit.uio.no>
2023-03-06 09:41:48 +0100

    Add CLI value style

--- harbor_cli/style.py
+++ harbor_cli/style.py
@@ -2,9 +2,14 @@
 from __future__ import annotations
 
 STYLE_CONFIG_OPTION = "italic yellow"
-"""The style used to signify a configuration file option/key/entry."""
+"""Style used to signify a configuration file option/key/entry."""
+
 STYLE_CLI_OPTION = "green"
-"""The style used to signify a CLI option, e.g. --verbose."""
+"""Style used to signify a CLI option, e.g. --verbose."""
+
+STYLE_CLI_VALUE = "bold magenta"
+"""Style used in command docstrings to signify a CLI value e.g. 'FILE'."""
+
 STYLE_TABLE_HEADER = "bold green"
 STYLE_COMMAND = "bold italic green"
 STYLE_WARNING = "yellow"
@@ -22,6 +27,11 @@ def render_config_option(option: str) -> str:
 def render_cli_option(option: str) -> str:
     """Render a CLI option."""
     return f"[{STYLE_CLI_OPTION}]{option}[/]"
+
+
+def render_cli_value(value: str) -> str:
+    """Render a CLI value."""
+    return f"[{STYLE_CLI_VALUE}]{value}[/]"
 
 
 def help_config_override(option: str) -> str:
