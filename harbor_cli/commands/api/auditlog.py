4e6a46e2ccd4febf136f0eaf9b230a3b14affee2
pederhan <pederhan@usit.uio.no>
2023-03-10 15:35:22 +0100

    Rename `auditlog purge` to `auditlog rotation`

--- harbor_cli/commands/api/auditlog.py
+++ harbor_cli/commands/api/auditlog.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+from typing import List
 from typing import Optional
 from typing import TYPE_CHECKING
+
+from ...utils.args import add_to_query
+from ...utils.args import parse_commalist
 
 if TYPE_CHECKING:
     from typing import Any  # noqa: F401
@@ -9,7 +13,7 @@ if TYPE_CHECKING:
 import typer
 from harborapi.models import Schedule, ScheduleObj
 
-from ...style import render_cli_value
+from ...style import render_cli_option, render_cli_value
 
 from ...output.render import render_result
 from ...state import state
@@ -19,32 +23,63 @@ from ...utils.prompts import check_enumeration_options
 # Create a command group
 app = typer.Typer(name="auditlog", help="System information", no_args_is_help=True)
 
-# harbor auditlog purge <subcommand>
-purge_cmd = typer.Typer(name="purge", help="Purge audit logs", no_args_is_help=True)
+# harbor auditlog rotation <subcommand>
+rotation_cmd = typer.Typer(name="rotation", help="Log rotation", no_args_is_help=True)
 
-# harbor auditlog purge schedule <subcommand>
+# harbor auditlog rotation schedule <subcommand>
 schedule_cmd = typer.Typer(
-    name="schedule", help="Purge audit log schedule", no_args_is_help=True
+    name="schedule", help="Log rotation schedule", no_args_is_help=True
 )
-purge_cmd.add_typer(schedule_cmd, name="schedule")
+rotation_cmd.add_typer(schedule_cmd)
 
-app.add_typer(purge_cmd, name="purge")
+app.add_typer(rotation_cmd)
 
 
 @app.command("list")
 @inject_resource_options()
 def list_audit_logs(
     ctx: typer.Context,
-    query: Optional[str],
-    sort: Optional[str],
-    page: int,
-    page_size: int,
-    limit: Optional[int],
+    operation: Optional[List[str]] = typer.Option(
+        None,
+        "--operation",
+        help=f"Operation(s) to filter audit logs by. E.g. {render_cli_value('create')}.",
+        callback=parse_commalist,
+    ),
+    resource: Optional[List[str]] = typer.Option(
+        None,
+        "--resource",
+        help=f"Full name of the resource(s) to filter by. E.g. {render_cli_value('library/foo:latest')}.",
+        callback=parse_commalist,
+    ),
+    resource_type: Optional[List[str]] = typer.Option(
+        None,
+        "--resource-type",
+        help=f"Resource type(s) to filter audit logs by. E.g. {render_cli_value('artifact')}.",
+        callback=parse_commalist,
+    ),
+    username: Optional[List[str]] = typer.Option(
+        None,
+        "--username",
+        help=f"Username to filter audit logs by.",
+        callback=parse_commalist,
+    ),
+    query: Optional[str] = ...,  # type: ignore
+    sort: Optional[str] = ...,  # type: ignore
+    page: int = ...,  # type: ignore
+    page_size: int = ...,  # type: ignore
+    limit: Optional[int] = typer.Option(10),
 ) -> None:
     """List audit logs for projects the current user has access to.
-    [bold red]WARNING:[/] This command can return a lot of data if no query or limit
-    is specified.
-    """
+    Recommended to specify a search query and to limit the number of results."""
+    # Add syntactic sugar options to query
+    query = add_to_query(
+        query,
+        operation=operation,
+        resource=resource,
+        resource_type=resource_type,
+        username=username,
+    )
+
     check_enumeration_options(state, query=query, limit=limit)
     logs = state.run(
         state.client.get_audit_logs(
@@ -59,12 +94,12 @@ def list_audit_logs(
     render_result(logs, ctx)
 
 
-# Purge commands
+# Log rotation commands
 
-# HarborAsyncClient.get_purge_audit_logs()
-@purge_cmd.command("list")
+# HarborAsyncClient.get_audit_log_rotation_history()
+@rotation_cmd.command("list")
 @inject_resource_options()
-def list_purge_audit_logs(
+def get_audit_log_rotation_history(
     ctx: typer.Context,
     query: Optional[str],
     sort: Optional[str],
@@ -72,71 +107,71 @@ def list_purge_audit_logs(
     page_size: int,
     limit: Optional[int],
 ) -> None:
-    """List purge audit logs."""
+    """List log rotation job logs."""
     check_enumeration_options(state, limit=limit)
     logs = state.run(
-        state.client.get_purge_audit_logs(
+        state.client.get_audit_log_rotation_history(
             query=query,
             sort=sort,
             page=page,
             page_size=page_size,
             limit=limit,
         ),
-        f"Fetching audit log purge results...",
+        f"Fetching log rotation results...",
     )
     render_result(logs, ctx)
 
 
-# HarborAsyncClient.get_purge_audit_log()
-@purge_cmd.command("get")
-def get_purge_audit_log(
+# HarborAsyncClient.get_audit_log_rotation()
+@rotation_cmd.command("get")
+def get_audit_log_rotation(
     ctx: typer.Context,
-    purge_id: int = typer.Argument(..., help="Purge audit log ID"),
+    job_id: int = typer.Argument(..., help="Log rotation job ID"),
 ) -> None:
-    """Get a purge audit log job."""
+    """Get a log rotation job."""
     result = state.run(
-        state.client.get_purge_audit_log(purge_id),
-        f"Fetching audit log purge result...",
+        state.client.get_audit_log_rotation(job_id),
+        f"Fetching log rotation result...",
     )
     render_result(result, ctx)
 
 
-# HarborAsyncClient.get_purge_audit_log_status()
-@purge_cmd.command("status")
-def get_purge_audit_log_status(
+# HarborAsyncClient.get_audit_log_rotation_log()
+@rotation_cmd.command("log")
+def get_audit_log_rotation_log(
     ctx: typer.Context,
-    purge_id: int = typer.Argument(..., help="Purge audit log ID"),
+    job_id: int = typer.Argument(..., help="Log rotation job ID"),
 ) -> None:
-    """Get a purge audit log job status."""
+    """Get the log for a log rotation job."""
     status = state.run(
-        state.client.get_purge_audit_log_status(purge_id),
-        f"Fetching audit log purge status...",
+        state.client.get_audit_log_rotation_log(job_id),
+        f"Fetching log rotation status...",
     )
     render_result(status, ctx)
 
 
-# HarborAsyncClient.stop_purge_audit_log()
-@purge_cmd.command("stop")
-def stop_purge_audit_log(
+# HarborAsyncClient.stop_audit_log_rotation()
+@rotation_cmd.command("stop")
+def stop_audit_log_rotation(
     ctx: typer.Context,
-    purge_id: int = typer.Argument(..., help="Purge audit log ID"),
+    job_id: int = typer.Argument(..., help="Log rotation job ID"),
 ) -> None:
-    """Stop a purge audit log job."""
+    """Stop a log rotation job."""
     state.run(
-        state.client.stop_purge_audit_log(purge_id),
-        f"Stopping audit log purge...",
+        state.client.stop_audit_log_rotation(job_id),
+        f"Stopping log rotation...",
     )
 
 
-# HarborAsyncClient.get_purge_audit_log_schedule()
+# HarborAsyncClient.get_audit_log_rotation_schedule()
 @schedule_cmd.command("get")
-def get_purge_audit_log_schedule(
+def get_audit_log_rotation_schedule(
     ctx: typer.Context,
 ) -> None:
-    """Get the purge audit log schedule."""
+    """Get the log rotation schedule."""
     schedule = state.run(
-        state.client.get_purge_audit_log_schedule(),
-        f"Fetching audit log purge schedule...",
+        state.client.get_audit_log_rotation_schedule(),
+        f"Fetching log rotation schedule...",
     )
     render_result(schedule, ctx)
 
@@ -168,27 +203,30 @@ def _get_schedule(
     )
 
 
-# HarborAsyncClient.create_purge_audit_log_schedule()
+# HarborAsyncClient.create_audit_log_rotation_schedule()
 @schedule_cmd.command("create")
-def create_purge_audit_log_schedule(
+def create_audit_log_rotation_schedule(
     ctx: typer.Context,
+    type: Optional[str] = typer.Option(
+        None,
+        help=f"Type of schedule, e.g. {render_cli_value('Hourly')}. Mutually exclusive with {render_cli_option('--cron')}.",
+    ),
     cron: Optional[str] = typer.Option(
         None, help=f"Cron expression, e.g. {render_cli_value('0 0 * * *')}"
     ),
     audit_retention_hour: Optional[str] = typer.Option(
         None,
+        "--retention-hours",
         help=f"Number of hours to retain audit logs, e.g. {render_cli_value('168')}",
     ),
+    # TODO: implement audit_retention_day if confirmed to work
     include_operations: Optional[str] = typer.Option(
         None,
-        help=f"Operations to purge logs for e.g. {render_cli_value('create,delete,pull')}",
+        help=f"Operations to rotate logs for e.g. {render_cli_value('create,delete,pull')}",
     ),
     dry_run: Optional[bool] = typer.Option(None, help="Dry run"),
-    type: Optional[str] = typer.Option(
-        None, help=f"Type of schedule, e.g. {render_cli_value('Hourly')}."
-    ),
 ) -> None:
-    """Create a purge audit log schedule."""
+    """Create an audit log rotation schedule."""
     schedule = _get_schedule(
         cron=cron,
         audit_retention_hour=audit_retention_hour,
@@ -198,31 +236,33 @@ def create_purge_audit_log_schedule(
     )
 
     s = state.run(
-        state.client.create_purge_audit_log_schedule(schedule),
-        f"Creating audit log purge schedule...",
+        state.client.create_audit_log_rotation_schedule(schedule),
+        f"Creating audit log rotation schedule...",
     )
     render_result(s, ctx)
 
 
-# HarborAsyncClient.update_purge_audit_log_schedule()
+# HarborAsyncClient.update_audit_log_rotation_schedule()
 @schedule_cmd.command("update")
-def update_purge_audit_log_schedule(
+def update_audit_log_rotation_schedule(
     ctx: typer.Context,
+    type: Optional[str] = typer.Option(
+        None,
+        help=f"Type of schedule, e.g. {render_cli_value('Hourly')}. Mutually exclusive with {render_cli_option('--cron')}.",
+    ),
     cron: Optional[str] = typer.Option(
         None, help=f"Cron expression, e.g. {render_cli_value('0 0 * * *')}"
     ),
     audit_retention_hour: Optional[str] = typer.Option(
         None,
+        "--retention-hours",
         help=f"Number of hours to retain audit logs, e.g. {render_cli_value('168')}",
     ),
     include_operations: Optional[str] = typer.Option(
         None,
-        help=f"Operations to purge logs for e.g. {render_cli_value('create,delete,pull')}",
+        help=f"Operations to rotate logs for e.g. {render_cli_value('create,delete,pull')}",
     ),
     dry_run: Optional[bool] = typer.Option(None, help="Dry run"),
-    type: Optional[str] = typer.Option(
-        None, help=f"Type of schedule, e.g. {render_cli_value('Hourly')}."
-    ),
 ) -> None:
     schedule = _get_schedule(
         cron=cron,
@@ -233,7 +273,7 @@ def update_purge_audit_log_schedule(
     )
 
     s = state.run(
-        state.client.update_purge_audit_log_schedule(schedule),
-        f"Creating audit log purge schedule...",
+        state.client.update_audit_log_rotation_schedule(schedule),
+        f"Updating audit log rotation schedule...",
     )
     render_result(s, ctx)
