8c54cc3e15133c3d42dbd7c8cceb0854809ed1aa
pederhan <pederhan@usit.uio.no>
2023-03-14 11:56:01 +0100

    Update sample config, ensure no double newline

--- harbor_cli/commands/cli/sample_config.py
+++ harbor_cli/commands/cli/sample_config.py
@@ -8,4 +8,4 @@ from ...output.console import console
 @app.command("sample-config")
 def sample_config() -> None:
     """Print a sample config file to stdout."""
-    console.print(get_sample_config(), markup=False)
+    console.print(get_sample_config(), markup=False, end="")
--- tests/commands/cli/test_sample_config.py
+++ tests/commands/cli/test_sample_config.py
@@ -4,3 +4,5 @@ from __future__ import annotations
 def test_sample_config(invoke):
     result = invoke(["sample-config"])
     assert result.exit_code == 0
+    assert result.stdout.endswith("\n")
+    assert not result.stdout.endswith("\n\n")
