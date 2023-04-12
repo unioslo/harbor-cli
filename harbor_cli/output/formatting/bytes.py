c11a893e54c426df4c1a57c24b4c95763ca670a0
pederhan <pederhan@usit.uio.no>
2023-04-12 11:48:15 +0200

    Allow None for `bytesize_str()`

--- harbor_cli/output/formatting/bytes.py
+++ harbor_cli/output/formatting/bytes.py
@@ -3,5 +3,5 @@ from __future__ import annotations
 from pydantic import ByteSize
 
 
-def bytesize_str(b: int, decimal: bool = False) -> str:
-    return ByteSize(b).human_readable(decimal=decimal)
+def bytesize_str(b: int | None, decimal: bool = False) -> str:
+    return ByteSize(b or 0).human_readable(decimal=decimal)
