559372b5416d05f013fca83d0128710ea5a36795
pederhan <pederhan@usit.uio.no>
2023-04-26 15:47:04 +0200

    Add remaining retention commands

--- harbor_cli/commands/api/retention.py
+++ harbor_cli/commands/api/retention.py
@@ -15,6 +15,7 @@ from ...state import state
 from ...style import render_cli_value
 from ...utils.args import get_project_arg
 from ...utils.commands import ARG_PROJECT_NAME_OR_ID_OPTIONAL
+from ...utils.commands import inject_resource_options
 from ...utils.commands import OPTION_FORCE
 
 app = typer.Typer(
@@ -22,12 +23,38 @@ app = typer.Typer(
     help="Artifact retention policy management and execution.",
     no_args_is_help=True,
 )
-policy_app = typer.Typer(
+policy_cmd = typer.Typer(
     name="policy",
-    help="Artifact retention policy.",
+    help="Retention policy commands.",
     no_args_is_help=True,
 )
-app.add_typer(policy_app)
+job_cmd = typer.Typer(
+    name="job",
+    help="Retention job commands.",
+    no_args_is_help=True,
+)
+task_cmd = typer.Typer(
+    name="task",
+    help="Retention job task commands.",
+    no_args_is_help=True,
+)
+job_cmd.add_typer(task_cmd)  # retention job task
+app.add_typer(job_cmd)  # retention job
+app.add_typer(policy_cmd)  # retention policy
+
+
+def policy_id_from_args(project_name_or_id: str | None, policy_id: int | None) -> int:
+    """Helper function for functions that either take in a project name/id or a policy ID.
+    Returns the policy ID."""
+    if policy_id is None and project_name_or_id is None:
+        exit_err(f"Must provide an argument or use {render_cli_value('--id')}.")
+
+    # Policy ID takes precedence
+    if policy_id is not None:
+        return policy_id
+    else:
+        assert project_name_or_id is not None  # should be impossible to fail
+        return get_retention_policy_id(project_name_or_id)
 
 
 def get_retention_policy_id(project_name_or_id: str) -> int:
@@ -50,42 +77,45 @@ def get_retention_policy(policy_id: int) -> RetentionPolicy:
     )
 
 
+OPTION_POLICY_ID = typer.Option(
+    None,
+    "--id",
+    help=f"Retention policy ID. Overrides positional argument.",
+)
+# HarborAsyncClient.get_retention_metadata()
+@app.command("metadata")
+def get_retention_metadata(ctx: typer.Context) -> None:
+    """Get the metadata for retentions."""
+    # NOTE: I have no idea what this does, so this description is a guess
+    metadata = state.run(
+        state.client.get_retention_metadata(),
+        "Fetching retention metadata...",
+    )
+    render_result(metadata, ctx)
+
+
 # HarborAsyncClient.get_retention_policy()
-@policy_app.command("get")
+@policy_cmd.command("get")
 def get_retention_policy_cmd(
     ctx: typer.Context,
     project_name_or_id: Optional[str] = ARG_PROJECT_NAME_OR_ID_OPTIONAL,
-    policy_id: Optional[int] = typer.Option(
-        None,
-        "--id",
-        help=f"Retention policy ID. Overrides positional argument.",
-    ),
+    policy_id: Optional[int] = OPTION_POLICY_ID,
 ) -> None:
-    """Fetch a retention policy given a project name or ID, [bold]or[/bold] using a retention policy ID."""
-    if policy_id is None and project_name_or_id is None:
-        exit_err(f"Must provide an argument or use {render_cli_value('--id')}.")
-    if project_name_or_id is not None:
-        policy_id = get_retention_policy_id(project_name_or_id)
-
-    assert policy_id is not None  # mypy...
-
+    """Fetch a retention policy."""
+    policy_id = policy_id_from_args(project_name_or_id, policy_id)
     policy = get_retention_policy(policy_id)
     render_result(policy, ctx)
 
 
 # HarborAsyncClient.delete_retention_policy()
-@policy_app.command("delete")
+@policy_cmd.command("delete")
 def delete_retention_policy(
     ctx: typer.Context,
     project_name_or_id: Optional[str] = ARG_PROJECT_NAME_OR_ID_OPTIONAL,
-    policy_id: Optional[int] = typer.Option(
-        None,
-        "--id",
-        help=f"Retention policy ID. Overrides positional argument.",
-    ),
+    policy_id: Optional[int] = OPTION_POLICY_ID,
     force: bool = OPTION_FORCE,
 ) -> None:
-    """Delete a retention policy given a project name or ID, [bold]or[/bold] using a retention policy ID.
+    """Delete a retention policy.
 
     !!! warning
         The user [bold]needs[/] permission to update the project metadata
@@ -93,15 +123,10 @@ def delete_retention_policy(
         API bug that will break a project if the project metadata is not updated
         after its retention policy is deleted.
     """
-    if policy_id is None and project_name_or_id is None:
-        exit_err(f"Must provide an argument or use {render_cli_value('--id')}.")
+    # Confirm deletion
+    delete_prompt(state.config, force, resource="retention policy")
 
-    if project_name_or_id is not None:
-        policy_id = get_retention_policy_id(project_name_or_id)
-
-    delete_prompt(state.config, force, resource="retention policy", name=str(policy_id))
-
-    assert policy_id is not None  # mypy...
+    policy_id = policy_id_from_args(project_name_or_id, policy_id)
 
     # We can potentially break a project here if we are able to delete the policy
     # but not update the project's metadata, so we need to backup the project metadata,
@@ -116,7 +141,7 @@ def delete_retention_policy(
         # get project ID from the retention policy if possible
         if policy.scope is None or policy.scope.ref is None:
             exit_err(
-                "Cannot update project metadata. Unable to determine project ID from retention policy. Use the project positional argument."
+                "Unable to determine project ID from retention policy. Specify project name or ID with the positional argument."
             )
         project_arg = policy.scope.ref
 
@@ -144,3 +169,124 @@ def delete_retention_policy(
         )
 
     logger.info(f"Deleted retention policy {policy_id}.")
+
+
+# HarborAsyncClient.get_retention_policy_id()
+@policy_cmd.command("id")
+def get_retention_policy_id_cmd(
+    ctx: typer.Context,
+    project_name_or_id: str = ARG_PROJECT_NAME_OR_ID_OPTIONAL,
+) -> None:
+    """Get the retention policy ID for a project."""
+    policy_id = get_retention_policy_id(project_name_or_id)
+    render_result(policy_id, ctx)
+
+
+# TODO: add these commands once the API is improved
+# HarborAsyncClient.create_retention_policy()
+# HarborAsyncClient.update_retention_policy()
+
+# HarborAsyncClient.get_retention_executions()
+@job_cmd.command("list")
+@inject_resource_options()
+def list_retention_jobs(
+    ctx: typer.Context,
+    project_name_or_id: Optional[str] = ARG_PROJECT_NAME_OR_ID_OPTIONAL,
+    policy_id: Optional[int] = OPTION_POLICY_ID,
+    page: int = ...,  # type: ignore
+    page_size: int = ...,  # type: ignore
+    limit: Optional[int] = ...,  # type: ignore
+) -> None:
+    """List retention jobs."""
+    policy_id = policy_id_from_args(project_name_or_id, policy_id)
+    jobs = state.run(
+        state.client.get_retention_executions(
+            policy_id, page=page, page_size=page_size, limit=limit
+        ),
+        "Fetching retention jobs...",
+    )
+    render_result(jobs, ctx)
+
+
+# HarborAsyncClient.start_retention_execution()
+@job_cmd.command("start")
+@inject_resource_options()
+def start_retention_job(
+    ctx: typer.Context,
+    project_name_or_id: Optional[str] = ARG_PROJECT_NAME_OR_ID_OPTIONAL,
+    policy_id: Optional[int] = OPTION_POLICY_ID,
+    dry_run: bool = typer.Option(
+        False,
+        "--dry-run",
+        help="Run job in dry run mode.",
+    ),
+) -> None:
+    """Start a retention job."""
+    policy_id = policy_id_from_args(project_name_or_id, policy_id)
+    location = state.run(
+        state.client.start_retention_execution(policy_id, dry_run),
+        "Starting retention job...",
+    )
+    msg = f"Started retention job at {location}."
+    if dry_run:
+        msg += " (Dry run)"
+    logger.info(msg)
+
+
+# HarborAsyncClient.stop_retention_execution()
+@job_cmd.command("stop")
+@inject_resource_options()
+def stop_retention_job(
+    ctx: typer.Context,
+    project_name_or_id: Optional[str] = ARG_PROJECT_NAME_OR_ID_OPTIONAL,
+    job_id: int = typer.Argument(..., help="ID of the job to stop."),
+    policy_id: Optional[int] = OPTION_POLICY_ID,
+) -> None:
+    """Stop a retention job."""
+    policy_id = policy_id_from_args(project_name_or_id, policy_id)
+    state.run(
+        state.client.stop_retention_execution(policy_id, job_id),
+        "Stopping retention job...",
+    )
+    logger.info(f"Stopped retention job ({job_id}) for policy {policy_id}.")
+
+
+# HarborAsyncClient.get_retention_tasks()
+@task_cmd.command("list")
+@inject_resource_options()
+def list_retention_tasks(
+    ctx: typer.Context,
+    project_name_or_id: Optional[str] = ARG_PROJECT_NAME_OR_ID_OPTIONAL,
+    job_id: int = typer.Argument(..., help="ID of the job to list tasks for."),
+    policy_id: Optional[int] = OPTION_POLICY_ID,
+    page: int = ...,  # type: ignore
+    page_size: int = ...,  # type: ignore
+    limit: Optional[int] = ...,  # type: ignore
+) -> None:
+    """List retention tasks."""
+    policy_id = policy_id_from_args(project_name_or_id, policy_id)
+    tasks = state.run(
+        state.client.get_retention_tasks(
+            policy_id, job_id, page=page, page_size=page_size, limit=limit
+        ),
+        "Fetching retention job tasks...",
+    )
+    render_result(tasks, ctx)
+
+
+# HarborAsyncClient.get_retention_execution_task_log()
+@task_cmd.command("log")
+def get_retention_job_task_log(
+    ctx: typer.Context,
+    project_name_or_id: Optional[str] = ARG_PROJECT_NAME_OR_ID_OPTIONAL,
+    job_id: int = typer.Argument(..., help="ID of job."),
+    task_id: int = typer.Argument(..., help="ID of job task."),
+    policy_id: Optional[int] = OPTION_POLICY_ID,
+) -> None:
+    """Get the log for a retention job task."""
+    policy_id = policy_id_from_args(project_name_or_id, policy_id)
+    log = state.run(
+        state.client.get_retention_execution_task_log(policy_id, job_id, task_id),
+        "Fetching retention job task log...",
+    )
+    render_result(log, ctx)
