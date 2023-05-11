849458d5abff235facf24b04eb8311527efd4ce0
pederhan <pederhan@usit.uio.no>
2023-05-11 16:31:11 +0200

    Remove unused command help patching

--- harbor_cli/commands/__init__.py
+++ harbor_cli/commands/__init__.py
@@ -8,10 +8,4 @@ groups = {
     "CLI": cli_commands,
 }
 
-# Patch help text for the different groups
-for group_name, command_groups in groups.items():
-    for command_group in command_groups:
-        # command_group.info.help = f"[{group_name}] {command_group.info.help or ''}"
-        pass
-
 ALL_GROUPS = [command for group in groups.values() for command in group]
