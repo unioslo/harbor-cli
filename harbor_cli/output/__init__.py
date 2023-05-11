53b5d208f86e3054ab67a774510a25dfb1bc37b6
pederhan <pederhan@usit.uio.no>
2023-05-11 16:19:09 +0200

    Remove wildcard import from console

--- harbor_cli/output/__init__.py
+++ harbor_cli/output/__init__.py
@@ -1,8 +1,3 @@
 from __future__ import annotations
 
 from . import *
-from .console import *
-from .formatting import *
-from .prompts import *
-from .render import *
-from .table import *  # type: ignore # just a typevar name collision
