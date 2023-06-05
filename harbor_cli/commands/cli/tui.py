b39eb7bf454b96ffa89656f011b53160afb6de6d
pederhan <pederhan@usit.uio.no>
2023-06-05 10:57:39 +0200

    Forbid launching TUI from within REPL

--- harbor_cli/commands/cli/tui.py
+++ harbor_cli/commands/cli/tui.py
@@ -5,6 +5,8 @@ from trogon import Trogon
 from typer.main import get_group
 
 from ...app import app
+from ...output.console import exit_err
+from ...state import get_state
 
 
 @app.command()
@@ -12,4 +14,9 @@ def tui(
     ctx: typer.Context,  # REPL options
 ) -> None:
     """Start a TUI (text-based user interface)."""
+    state = get_state()
+    if state.repl:
+        exit_err(
+            f"Cannot launch TUI from REPL mode. Exit REPL and run [italic]harbor tui[/italic]."
+        )
     Trogon(get_group(app), click_context=ctx, app_name="harbor").run()
