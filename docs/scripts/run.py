76a169f50254dae4e6deb6290d25e54a4d21ab45
pederhan <pederhan@usit.uio.no>
2023-08-07 15:15:33 +0200

    Docs: remove main() function in script runner

--- docs/scripts/run.py
+++ docs/scripts/run.py
@@ -12,16 +12,16 @@ from . import gen_command_pages  # noqa
 from . import gen_formats  # noqa
 
 
-def main(*args, **kwargs) -> None:
-    for mod in [
-        gen_cli_data,
-        gen_cli_options,
-        gen_command_list,
-        gen_command_pages,
-        gen_formats,
-    ]:
-        mod.main()
+# NOTE: for some reason putting this logic into a main() function causes the first
+# build to not actually include all the generated files, while just putting it
+# at the top-level works fine (albeit with a warning about a missing main() function)
+# Not sure why, but it's not worth investigating right now.
 
-
-if __name__ == "__main__":
-    main()
+for mod in [
+    gen_cli_data,
+    gen_cli_options,
+    gen_command_list,
+    gen_command_pages,
+    gen_formats,
+]:
+    mod.main()
