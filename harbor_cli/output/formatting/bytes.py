7c428b297326cc1ca5a6f1734013ba8ed0cb09ba
pederhan <pederhan@usit.uio.no>
2023-08-29 12:37:30 +0200

    Don't format `None` as `"0.0B"`
    A quota of `None` should not be shown as an actual byte size.

--- harbor_cli/output/formatting/bytes.py
+++ harbor_cli/output/formatting/bytes.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from pydantic import ByteSize
 
+from .constants import NONE_STR
+
 
 def bytesize_str(b: int | None, decimal: bool = False) -> str:
-    return ByteSize(b or 0).human_readable(decimal=decimal)
+    return ByteSize(b).human_readable(decimal=decimal) if b is not None else NONE_STR
