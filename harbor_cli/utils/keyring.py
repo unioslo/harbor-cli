54775f5b7c7fe312e9584a3d799dbfbe77de45e9
pederhan <pederhan@usit.uio.no>
2024-02-02 12:44:08 +0100

    Handle keyring errors in exceptions.py

--- harbor_cli/utils/keyring.py
+++ harbor_cli/utils/keyring.py
@@ -1,28 +1,23 @@
 from __future__ import annotations
 
 import functools
-import sys
 from typing import Callable
 from typing import TYPE_CHECKING
 from typing import TypeVar
 
 import keyring
 from keyring.errors import KeyringError
-from keyring.errors import PasswordDeleteError
-from keyring.errors import PasswordSetError
+from keyring.errors import NoKeyringError
 from typing_extensions import ParamSpec
 
 from harbor_cli.exceptions import KeyringUnsupportedError
 from harbor_cli.logs import logger
 from harbor_cli.output.console import info
-from harbor_cli.output.console import warning
 
 if TYPE_CHECKING:
     from keyring.backend import KeyringBackend
 
 KEYRING_SERVICE_NAME = "harbor_cli"
-_KEYRING_DUMMY_USER = "test_user"
-_KEYRING_DUMMY_PASSWORD = "test_password"
 
 
 class DummyPasswordNoMatchError(Exception):
@@ -38,51 +33,16 @@ def keyring_supported() -> bool:
     try:
         backend = get_backend()
         logger.debug("Using keyring backend: %s", backend)
+        return True
+    except NoKeyringError:
+        pass  # does not need to be logged
     except KeyringError as e:
-        if sys.platform == "darwin":
-            if (
-                isinstance(e, (PasswordSetError, PasswordDeleteError))
-                and e.__context__
-                and e.__context__.args
-                and e.__context__.args[0] == -25244
-            ):
-                return _handle_keyring_error_25244_macos(e)
-        else:
-            logger.error("Keyring error: %s", e)
+        logger.error("Keyring error: %s", e, exc_info=True)
         return False
     except Exception as e:
         logger.error(
             "Unknown error when checking keyring availability: %s", e, exc_info=True
         )
-        return False
-    else:
-        return True
-
-
-def _handle_keyring_error_25244_macos(e: KeyringError) -> bool:
-    """Handles macOS keyring error -25244 (errSecInvalidOwnerEdit error) when setting
-    a password
-
-    This very likely happens if we try to access the keyring from an application
-    that is not signed with the same certificate as the one that created the keychain
-    item initially.
-
-    See: https://developer.apple.com/forums/thread/69841
-    See: https://github.com/python-poetry/poetry/issues/2692#issuecomment-1382387632
-    """
-    actions = {
-        PasswordSetError: "set",
-        PasswordDeleteError: "delete",
-    }
-    action = actions.get(type(e), "access")
-
-    warning(
-        f"An error occurred while trying to {action} a password in your keyring.\n"
-        f"Please delete any passwords related to [i]{KEYRING_SERVICE_NAME}[/] from your keyring "
-        "and try again.\n"
-        "If this issue persists, please report it and disable keyring in your config file.\n"
-        "Run [i]harbor cli-config path[/] to find the location of your config file."
-    )
     return False
 
 
